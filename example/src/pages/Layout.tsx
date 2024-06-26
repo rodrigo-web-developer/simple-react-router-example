4a8bc8288290d333520cd1b8e7ac38c9b9eedd5a
rodrigogoncalves <rodrigogoncalves@interfocus.com.br>
2024-06-26 14:56:54 -0300

    classname attribute example added

--- example/src/pages/Layout.tsx
+++ example/src/pages/Layout.tsx
@@ -17,12 +17,12 @@ export default function Layout() {
     return (<div className="layout">
         <nav>
             <ul>
-                <li><Link to="/pagina1">IR PARA PAGINA 1</Link></li>
+                <li><Link className="my-class" to="/pagina1">IR PARA PAGINA 1</Link></li>
                 <li><Link to="/pagina2">IR PARA PAGINA 2</Link></li>
                 <li><Link to="/pagina3">IR PARA PAGINA 3</Link></li>
                 <li><Link to="/rota/textonormal">PAGINA COM REGEX</Link></li>
                 <li><Link to="/rota/123456">PAGINA COM REGEX</Link></li>
-                <li><Link to="/rota/teste">PAGINA SEM REGEX</Link></li>
+                <li><Link className="my-class" to="/rota/teste">PAGINA SEM REGEX</Link></li>
                 <li><a href="" onClick={(e) => redirectToProduct(e)}>PAGINA REGEX TIPO GUID E NUMBER</a></li>
                 <li><Link to="/404">PAGINA SEM REGISTRO</Link></li>
 
